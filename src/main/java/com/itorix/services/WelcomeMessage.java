3e7ae6d5a0ce0bf51cc6095dbbf1f2d36d2e9316
saitejDev <saiteja.tanniru@whizzard.in>
2023-02-16 15:06:36 +0530

    newWishMethod

--- src/main/java/com/itorix/services/WelcomeMessage.java
+++ src/main/java/com/itorix/services/WelcomeMessage.java
@@ -4,4 +4,8 @@ public class WelcomeMessage {
     public String getWishMessage(){
         return "welcome to hydrabad";
     }
+
+    public String getWishMessage(String name){
+        return "welcome to hydrabad Mr:"+name;
+    }
 }
